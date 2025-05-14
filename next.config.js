031261fc687d342777c69ffaf40b3e291a1d0753
Peter Pressman <peterpressman@Peters-MacBook-Pro-3.local>
2025-05-13 19:48:45 -0700

    Fix build configuration for Vercel

--- next.config.js
+++ next.config.js
@@ -1,1 +1,10 @@
-module.exports = {reactStrictMode: true,}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Change the output directory for production builds
+  distDir: 'out',
+  // This enables static HTML export
+  output: 'export'
+}
+
+module.exports = nextConfig
